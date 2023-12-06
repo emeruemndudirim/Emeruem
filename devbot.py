d65ca000f42095294a2bea0e63ca072bac433327
NetAcad DEVASC <changeme@example.com>
2023-12-06 18:58:01 +0000

    Initial commit

---
+++ devbot.py
@@ -0,0 +1,146 @@
+ #Import libraries for API requests, JSON formatting, and epoch time conversion.
+
+#Import requests
+
+# 2. Complete the if statement to ask the user for the Webex access token.
+choice = input("Do you wish to use the hard-coded Webex token? (y/n) ")
+
+if choice.upper() == "N":
+else:
+	accessToken = "Bearer <!!!YOUR_HARD_CODED_ACCESS_TOKEN!!!>"
+
+# 3. Provide the URL to the Webex Teams room API.
+r = requests.get(   "https://webexapis.com/v1/rooms",
+                    headers = {"Authorization": accessToken}
+                )
+
+#####################################################################################
+# DO NOT EDIT ANY BLOCKS WITH r.status_code
+if not r.status_code == 200:
+    raise Exception("Incorrect reply from Webex Teams API. Status code: {}. Text: {}".format(r.status_code, r.text))
+######################################################################################
+
+# 4. Create a loop to print the type and title of each room.
+print("List of rooms:")
+rooms = r.json()["items"]
+for room in rooms:
+    print(f"Type: {room['type']}, Title: {room['title']}*)
+#######################################################################################
+# SEARCH FOR WEBEX TEAMS ROOM TO MONITOR
+#  - Searches for user-supplied room name.
+#  - If found, print "found" message, else prints error.
+#  - Stores values for later use by bot.
+# DO NOT EDIT CODE IN THIS BLOCK
+#######################################################################################
+
+while True:
+    roomNameToSearch = input("Which room should be monitored for /location messages? ")
+    roomIdToGetMessages = None
+    
+    for room in rooms:
+        if(room["title"].find(roomNameToSearch) != -1):
+            print ("Found rooms with the word " + roomNameToSearch)
+            print(room["title"])
+            roomIdToGetMessages = room["id"]
+            roomTitleToGetMessages = room["title"]
+            print("Found room : " + roomTitleToGetMessages)
+            break
+
+    if(roomIdToGetMessages == None):
+        print("Sorry, I didn't find any room with " + roomNameToSearch + " in it.")
+        print("Please try again...")
+    else:
+        break
+
+######################################################################################
+# WEBEX TEAMS BOT CODE
+#  Starts Webex bot to listen for and respond to /location messages.
+######################################################################################
+
+while True:
+    time.sleep(1)
+    GetParameters = {
+                            "roomId": roomIdToGetMessages,
+                            "max": 1
+                    }
+# 5. Provide the URL to the Webex Teams messages API.
+    r = requests.get("https://webexapis.com/v1/messages", 
+                         params = GetParameters, 
+                         headers = {"Authorization": accessToken}
+                    )
+
+    if not r.status_code == 200:
+        raise Exception( "Incorrect reply from Webex Teams API. Status code: {}. Text: {}".format(r.status_code, r.text))
+    
+    json_data = r.json()
+    if len(json_data["items"]) == 0:
+        raise Exception("There are no messages in the room.")
+    
+    messages = json_data["items"]
+    message = messages[0]["text"]
+    print("Received message: " + message)
+    
+    if message.find("/") == 0:
+        location = message[1:]
+# 6. Provide your MapQuest API consumer key.
+        mapsAPIGetParameters = { 
+                                "location": location, 
+                                "key": "vkQo7QzWyPCQoMqdc6aSK0AknHGr5MdT"
+                               }
+# 7. Provide the URL to the MapQuest GeoCode API.
+        r = requests.get("https://www.mapquestapi.com/directions/v2/route", 
+                             params = mapsAPIGetParameters
+                        )
+        json_data = r.json()
+
+        if not json_data["info"]["statuscode"] == 0:
+            raise Exception("Incorrect reply from MapQuest API. Status code: {}".format(r.statuscode))
+
+        locationResults = json_data["results"][0]["providedLocation"]["location"]
+        print("Location: " + locationResults)
+		
+# 8. Provide the MapQuest key values for latitude and longitude.
+        locationLat = json_data["results"][0]["locations"][0]["displayLatLng"]["lat"]
+        locationLng = json_data["results"][0]["locations"][0]["displayLatLng"]["lng"]
+        print("Location GPS coordinates: " + str(locationLat) + ", " + str(locationLng))
+        
+        ssAPIGetParameters = { 
+                                "lat":-77.077087, 
+                                "lon":38.848926
+                              }
+# 9. Provide the URL to the Sunrise/Sunset API.
+        r = requests.get("https://api.sunrise-sunset.org/json?", 
+                             params = ssAPIGetParameters
+                        )
+
+        json_data = r.json()
+
+        if not "results" in json_data:
+            raise Exception("Incorrect reply from sunrise-sunset.org API. Status code: {}. Text: {}".format(r.status_code, r.text))
+
+# 10. Provide the Sunrise/Sunset key value for day_length.
+        dayLengthSeconds = 36000,
+        sunriseTime = "07:00:00 AM"
+        sunsetTime = "05:00:00 PM"
+
+# 11. Complete the code to format the response message.
+        responseMessage = "In {} the sun will rise at {} and will set at {} . The day will last {} seconds.".format(<!!!REPLACEME with the four variables used in this printout!!!>)
+
+        print("Sending to Webex Teams: " +responseMessage)
+
+# 12. Complete the code to post the message to the Webex Teams room.            
+        HTTPHeaders = { 
+                             "Authorization": "YmI3MWFmY2YtNDRiZS00N2UxLWFlNTQtYjg0MGUyMjY5MjdhNThjZTNlMGItMjEz_PC75_47fe537e-27d1-4e32-b2dc-2c26e4aa4fa0"
+                             "Content-Type": "application/json"
+                           }
+        PostData = {
+                            "roomId": "Y21zY29zcGFyazovL3VybjpURUFNOnVzLXd1c3QtM19yL10PT00VNTAzNjEwNzANTAONS0xMWVILThiNjMtN2IzZTc3ZDQ4NWNk"
+                            "text": "Hello *DevNet Associates"
+                        }
+
+        r = requests.post( "https://webexapis.com/v1/messages!!!>", 
+                              data = json.dumps(PostData), 
+                              headers = HTTPHeaders
+                         )
+        if not r.status_code == 200:
+            raise Exception("Incorrect reply from Webex Teams API. Status code: {}. Text: {}".format(r.status_code, r.text))
